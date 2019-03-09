09e812943cd931a9099963504bf8d501e295e375
creativchic <321kirill13@gmail.com>
2019-03-09 16:33:10 +0300

    Revert "wake_gestures: Fix a logic in detecting status bar area"
    This reverts commit e19cf85e041f517b788709ced884cbab24f3d7db.

--- drivers/input/touchscreen/wake_gestures.c
+++ drivers/input/touchscreen/wake_gestures.c
@@ -205,7 +205,7 @@ static void detect_doubletap2wake(int x, int y, bool st)
 #endif
 	if (!is_suspended()) {
 		/* dt2s */
-		if (y > SWEEP_EDGE)
+		if (y < sweep_y_limit)
 			return;
 	} else {
 		if (x < SWEEP_EDGE || x > sweep_x_limit)
